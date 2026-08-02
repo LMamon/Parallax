5f426f656ec95cf686d895d57c19143a9031e65f
Louis Mamon <Louis M>
2026-08-02 00:35:56 -0800

    refactor: encapsulate v4l2 file descriptor

--- ros_ws/src/px_camera/src/arducam_device.cpp
+++ ros_ws/src/px_camera/src/arducam_device.cpp
@@ -1,4 +1,6 @@
 #include "px_camera/arducam_device.hpp"
+#include <px_camera/v4l2_device.hpp>
+#include <px_camera/logger.hpp>
 
 #include <cerrno>
 #include <cstring>
@@ -10,7 +12,7 @@ namespace px_camera {
     
     std::uint16_t ArducamDevice::readSensor(std::uint16_t reg) const {
         if (!isOpen()) {
-            std::cerr << "Cant read sensor register: device is not open\n";
+            logMessage("Can't read sensor register: device is not open");
             return 0;
         }
 
@@ -18,8 +20,8 @@ namespace px_camera {
         ArducamI2C data{};
         data.reg = reg;
 
-        if (::ioctl(fd_, VIDIOC_R_I2C, &data) < 0) {
-            std::cerr << "Sensor register read failed: " << std::strerror(errno) << "\n";
+        if (::ioctl(fileDescriptor(), VIDIOC_R_I2C, &data) < 0) {
+            logError("VIDIOC_R_I2C");
             return 0;
         }
         return data.val;
@@ -27,7 +29,7 @@ namespace px_camera {
 
     bool ArducamDevice::writeSensor(std::uint16_t reg, std::uint16_t value) const {
         if (!isOpen()) {
-            std::cerr << "Cant write sensor register: device is not open\n";
+            logMessage("Can't write sensor register: device is not open");
             return false;
         }
 
@@ -35,8 +37,8 @@ namespace px_camera {
         data.reg = reg;
         data.val = value;
 
-        if (::ioctl(fd_, VIDIOC_W_I2C, &data) < 0) {
-            std::cerr << "Sensor register write failed: " << std::strerror(errno) << '\n';
+        if (::ioctl(fileDescriptor(), VIDIOC_W_I2C, &data) < 0) {
+            logError("VIDIOC_W_I2C");
             return false;
         }
         return true;
@@ -44,7 +46,7 @@ namespace px_camera {
 
     std::uint32_t ArducamDevice::readDevice(std::uint16_t reg) const {
         if (!isOpen()) {
-            std::cerr << "Cant read Arducam register: device is not open\n";
+            logMessage("Can't read Arducam register: device is not open");
             return 0;
         }
         //device registers belong to the Arducam controller/firmware,
@@ -52,8 +54,8 @@ namespace px_camera {
         ArducamDev data{};
         data.reg = reg;
 
-        if (::ioctl(fd_, VIDIOC_R_DEV, &data) < 0) {
-            std::cerr << "Arducam register read failed: " << std::strerror(errno) << "\n";
+        if (::ioctl(fileDescriptor(), VIDIOC_R_DEV, &data) < 0) {
+            logError("VIDIOC_R_DEV");
             return 0;
         }
         return data.val;
@@ -61,7 +63,7 @@ namespace px_camera {
 
     bool ArducamDevice::writeDevice(std::uint16_t reg, std::uint32_t value) const {
         if (!isOpen()) {
-            std::cerr << "Cant write Arducam register: device is not open\n";
+            logMessage("Can't write Arducam register: device is not open");
             return false;
         }
 
@@ -69,8 +71,8 @@ namespace px_camera {
         data.reg = reg;
         data.val = value;
 
-        if (::ioctl(fd_, VIDIOC_W_DEV, &data) < 0) {
-            std::cerr << "Arducam register write failed: " << std::strerror(errno) << "\n";
+        if (::ioctl(fileDescriptor(), VIDIOC_W_DEV, &data) < 0) {
+            logError("VIDIOC_W_DEV");
             return false;
         }
         return true;

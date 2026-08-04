b0884a037b540a5701b4ab225d7a593d3cf50d23
Louis Mamon <Louis M>
2026-08-03 19:36:04 -0800

    chore: cleaning out ros2 packages

--- ros_ws/src/px_camera/include/px_camera/logger.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-namespace px_camera {
-    void logError(const char* operation);
-    void logError(const char* operation, const char* object);
-    void logMessage(const char* message);
-}

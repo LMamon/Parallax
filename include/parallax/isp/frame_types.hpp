087fdab08d0391725970e11532a49fa4d28b606b
Louis Mamon <Louis M>
2026-08-14 15:14:01 -0800

    feat: add frame types for stereo disparity support

--- include/parallax/isp/frame_types.hpp
+++ include/parallax/isp/frame_types.hpp
@@ -13,7 +13,8 @@ namespace parallax::isp {
         RGBA8,
         Float32,
         Disparity16,
-        Depth32F
+        Depth32F,
+        Gray8
     };
 
     struct GpuBayerFrame {
@@ -35,6 +36,17 @@ namespace parallax::isp {
         static constexpr std::uint32_t Channels = 3;
         static constexpr PixelFormat Format = PixelFormat::RGB8;
     };
+    
+    struct StereoGrayFrame {
+        parallax::cuda::CudaBuffer left;
+        parallax::cuda::CudaBuffer right;
+
+        std::uint32_t width = 0;
+        std::uint32_t height = 0;
+
+        static constexpr std::uint32_t Channels = 1;
+        static constexpr PixelFormat Format = PixelFormat::Gray8;
+    };
 
     struct RectifiedStereoFrame {
         parallax::cuda::CudaBuffer left;
@@ -44,12 +56,29 @@ namespace parallax::isp {
         std::uint32_t height = 0;
 
         static constexpr std::uint32_t Channels = 3;
+        static constexpr PixelFormat Format = PixelFormat::RGB8;
     };
 
-    struct DisparityFrame {
-        parallax::cuda::CudaBuffer image;
+    struct RectifiedStereoGrayFrame {
+        parallax::cuda::CudaBuffer left;
+        parallax::cuda::CudaBuffer right;
 
         std::uint32_t width = 0;
         std::uint32_t height = 0;
+
+        static constexpr std::uint32_t Channels = 1;
+        static constexpr PixelFormat Format = PixelFormat::Gray8;
+    };
+    
+    struct StereoMatchFrame {
+        parallax::cuda::CudaBuffer disparity;
+        parallax::cuda::CudaBuffer confidence;
+        
+        std::uint32_t width = 0;
+        std::uint32_t height = 0;
+
+        // VPI S16 disparity is Q10.5 fixed point:
+        // disparity_pixels = stored_value / 32.0f
+        static constexpr float DisparityScale = 32.0f;
     };
 }

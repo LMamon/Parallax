e20b059051de19e05d153c81b7268a430f25937b
Louis Mamon <Louis M>
2026-08-14 16:13:27 -0800

    fea: add vpi wrappers for cuda image buffers

---
+++ src/vpi/image_wrapper.cpp
@@ -0,0 +1,64 @@
+#include <parallax/vpi/image_wrapper.hpp>
+
+#include <iostream>
+#include <utility>
+
+namespace parallax::vpi {
+    ImageWrapper::~ImageWrapper() { release(); }
+    ImageWrapper::ImageWrapper(ImageWrapper&& other) noexcept : image_(std::exchange(other.image_, nullptr)) {}
+    
+    ImageWrapper& ImageWrapper::operator=(ImageWrapper&& other) noexcept {
+        if (this != &other) {
+            release();
+            image_ = std::exchange(other.image_, nullptr);
+        }
+        return *this;
+    }
+
+    bool ImageWrapper::create(const parallax::cuda::CudaBuffer& buffer, VPIImageFormat format) {
+        release();
+
+        if (!buffer.isAllocated()) {
+            std::cerr << "Cannot wrap unallocated CUDA buffer\n";
+            return false;
+        }
+
+        VPIImageData data{};
+        data.bufferType = VPI_IMAGE_BUFFER_CUDA_PITCH_LINEAR;
+
+        auto& pitch = data.buffer.pitch;
+
+        pitch.format = format;
+        pitch.numPlanes = 1;
+
+        pitch.planes[0].data = const_cast<void*>(buffer.data());
+        pitch.planes[0].width = static_cast<int32_t>(buffer.width());
+        pitch.planes[0].height = static_cast<int32_t>(buffer.height());
+        pitch.planes[0].pitchBytes = static_cast<int32_t>(buffer.pitch());
+
+        VPIImageWrapperParams params {};
+        VPIStatus status = vpiInitImageWrapperParams(&params);
+
+        if (status != VPI_SUCCESS) {
+            std::cerr << "Failed to initialize VPI image wrapper parameters\n";
+            return false;
+        }
+
+        if (format == VPI_IMAGE_FORMAT_Y8_ER) params.colorSpec = VPI_COLOR_SPEC_BT601_ER;
+
+        status = vpiImageCreateWrapper(&data, &params, VPI_BACKEND_CUDA, &image_);
+        if (status != VPI_SUCCESS) {
+            std::cerr << "Failed to create VPI image wrapper\n";
+            image_ = nullptr;
+            return false;
+        }
+        return true;
+    }
+
+    void ImageWrapper::release() noexcept {
+        if (image_ != nullptr){
+            vpiImageDestroy(image_);
+            image_ = nullptr;
+        }
+    }
+}

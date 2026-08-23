6d4252eb60a33907f2bcf3966be431c1b747a92d
Louis Mamon <Louis M>
2026-08-22 18:47:17 -0800

    feat: depthframe for pose estimation/projection

--- include/parallax/isp/frame_types.hpp
+++ include/parallax/isp/frame_types.hpp
@@ -81,4 +81,13 @@ namespace parallax::isp {
         // disparity_pixels = stored_value / 32.0f
         static constexpr float DisparityScale = 32.0f;
     };
+
+    struct DepthFrame {
+        parallax::cuda::CudaBuffer depth;
+
+        std::uint32_t width = 0;
+        std::uint32_t height = 0;
+
+        static constexpr PixelFormat Format = PixelFormat::Depth32F;
+    };
 }

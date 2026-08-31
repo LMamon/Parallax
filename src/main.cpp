ed0ff420d319cec61b9278bc464d0413af0310c3
Louis Mamon <Louis M>
2026-08-30 17:07:20 -0800

    fix: typo

--- src/main.cpp
+++ src/main.cpp
@@ -15,14 +15,14 @@ int main() {
     parallax::core::Runtime runtime;
     const std::filesystem::path camera_config_path = "config/camera/stereo.yaml";
     const std::filesystem::path sensor_extrinsics_path = "config/sensors/extrinsics.yaml";
-    const std::filesystem::path nanoowl_engine_path = "models/nanoowl/owl_image_encoder_patch32.engine";
+    const std::filesystem::path nanoowl_engine_path = "models/owl_image_encoder_patch32_fp16.engine";
     const std::filesystem::path calibration_directory = "config/camera/calibration/results/rectification";
 
     if (!runtime.initialize(camera_config_path,
                             sensor_extrinsics_path, 
                             calibration_directory, 
                             nanoowl_engine_path)) {
-                                
+
         return EXIT_FAILURE;
     }
 
